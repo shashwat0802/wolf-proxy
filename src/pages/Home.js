6925067b7d43856382c76b1401a7bae4fb7c4855
shashwat gupta <shashwatgupta4656@gmail.com>
2021-02-24 23:16:27 +0530

    finish

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
           </button>
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav ml-auto ls-2">
               <li class="nav-item ">
                 <Link
                   class="nav-link text-white font-weight-bolder mx-3"
@@ -64,7 +64,7 @@ const Home = () => {
                 </Link>
               </li>
             </ul>
-            <button className="  btn primary-btn px-4 text-white ">
+            <button className="  btn primary-btn px-4 text-white ls-2 ">
               Dashboard
             </button>
           </div>
@@ -73,7 +73,7 @@ const Home = () => {
       {/* 1st section */}
       <section className="section-main">
         <div className="coantiner d-flex justify-content-around align-items-center">
-          <div>
+          <div className="ls-2">
             <h1 className="fs-50 primary-color">WOLF PROXIES</h1>
             <h2 className="secondary-color text-uppercase fs-20 my-2">
               Join the pack
@@ -87,10 +87,10 @@ const Home = () => {
       </section>
       {/* feature section */}
       <section className="p-5">
-        <h1 className="text-center font-weight-bolder primary-color fs-50 text-uppercase">
+        <h1 className="text-center font-weight-bolder primary-color fs-50 text-uppercase ls-2">
           Features
         </h1>
-        <div className="container mt-5">
+        <div className="container mt-5 ls-2">
           <div className="row">
             <div className="col-12 col-md-6">
               <div className="feature-box d-flex p-3">
@@ -150,15 +150,15 @@ const Home = () => {
         </div>
       </section>
       {/* plans section */}
-      <div className="plan-section p-5">
-        <h1 className="text-center font-weight-bolder primary-color fs-50 text-uppercase mb-5">
+      <section className="plan-section p-5 ">
+        <h1 className="text-center font-weight-bolder primary-color fs-50 text-uppercase mb-5 ls-2">
           plans
         </h1>
-        <div className="ml-auto d-flex justify-content-stretch button-wrapper text-center mb-5 mt-4">
-          <button className="btn button-active text-uppercase font-weight-bolder tertiary-color fs-22">
+        <div className="ml-auto d-flex justify-content-stretch button-wrapper text-center mb-5 mt-4 ">
+          <button className="btn button-active text-uppercase font-weight-bolder tertiary-color fs-22 ls-2">
             residential
           </button>
-          <button className="btn button-inactive text-uppercase font-weight-bolder tertiary-color fs-22 text-center">
+          <button className="btn button-inactive text-uppercase font-weight-bolder tertiary-color fs-22 text-center ls-2">
             isp
           </button>
         </div>
@@ -176,7 +176,10 @@ const Home = () => {
                   <button className="btn tertiary-color  secondary-btn font-weight-bolder px-3 py-1 my-4">
                     Residential
                   </button>
-                  <ul className="w-50 mx-auto text-left custom-list my-4">
+                  <ul
+                    className=" mx-auto text-left custom-list my-4 ls-1"
+                    style={{ width: '60% ' }}
+                  >
                     <li className="text-white my-4">- 99.9% Uptime</li>
                     <li className="text-white my-4">
                       - User/Pass Authentication
@@ -190,6 +193,7 @@ const Home = () => {
                     <li className="text-white my-4">
                       - Fresh IPs For Each Drop
                     </li>
+                    <li className="text-white my-4">- 30 Day Expiry</li>
                     <li className="text-white my-4">- Unbanned on All Sites</li>
                     <li className="text-white my-4">- Unlimited Generation</li>
                   </ul>
@@ -222,7 +226,78 @@ const Home = () => {
                     </p>
                   </div>
                   <button
-                    className="tertiary-color fs-20 text-uppercase btn button-wrapper px-5 py-2 font-weight-bolder mt-5"
+                    className="tertiary-color fs-20 text-uppercase btn button-wrapper px-5 py-2 font-weight-bolder mt-5 ls-2"
+                    style={{ width: '250px' }}
+                  >
+                    Sold Out
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* right side */}
+            <div className="col-12 col-md-6">
+              <div className="plans-wrapper py-5 px-4">
+                <div className="container text-center">
+                  <h1
+                    className="fs-35 tertiary-color"
+                    style={{ letterSpacing: '0.2em' }}
+                  >
+                    WOLF LUNAR
+                  </h1>
+                  <button className="btn tertiary-color  secondary-btn font-weight-bolder px-3 py-1 my-4">
+                    Residential
+                  </button>
+                  <ul
+                    className=" mx-auto text-left custom-list my-4"
+                    style={{ width: '60% ' }}
+                  >
+                    <li className="text-white my-4">- 99.9% Uptime</li>
+                    <li className="text-white my-4">
+                      - Real Worldwide Residential IPs
+                    </li>
+                    <li className="text-white my-4">- Massive Proxy Pools</li>
+                    <li className="text-white my-4">
+                      - Custom ISP Specific Pools
+                    </li>
+                    <li className="text-white my-4">
+                      - Country, State, and City Targeting
+                    </li>
+                    <li className="text-white my-4">- 60 Day Expiry </li>
+                    <li className="text-white my-4">
+                      - Unbanned on All Sites{' '}
+                    </li>
+                    <li className="text-white my-4">- Unlimited Generation</li>
+                  </ul>
+                  <div className="d-flex justify-content-center">
+                    <div
+                      className="d-flex button-wrapper tertiary-color fs-24 align-items-center "
+                      style={{ width: '150px' }}
+                    >
+                      <button
+                        className="btn font-weight-bolder text-white  "
+                        style={{ width: '30%' }}
+                      >
+                        <img src={require('../img/Minus.svg').default} alt="" />
+                      </button>
+                      <p
+                        className="m-0 font-weight-bolder "
+                        style={{ width: '40%' }}
+                      >
+                        2GB
+                      </p>
+                      <button
+                        className="btn font-weight-bolder text-white  "
+                        style={{ width: '30%' }}
+                      >
+                        <img src={require('../img/Plus.svg').default} alt="" />
+                      </button>
+                    </div>
+                    <p className="fs-33 mx-3 my-0 tertiary-color font-weight-bolder">
+                      $40
+                    </p>
+                  </div>
+                  <button
+                    className="tertiary-color fs-20 text-uppercase btn button-wrapper px-5 py-2 font-weight-bolder mt-5 ls-2"
                     style={{ width: '250px' }}
                   >
                     Sold Out
@@ -232,7 +307,99 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      {/* faq section */}
+      <section>
+        <div className="container">
+          <h1 className="text-center fs-50 secondary-color font-weight-bolder ls-2 my-5">
+            FAQ
+          </h1>
+          <h1 className="text-center fs-35 secondary-color font-weight-bolder ls-2 mb-5">
+            Frequently Asked Questions
+          </h1>
+          <div className="row my-5">
+            <div className="col-12 col-md-6 ">
+              <div className="faq-wrapper p-4">
+                <h3 className="text-white fs-20 font-weight-bold ls-2 ">
+                  How do I join Wolf Proxies?
+                </h3>
+                <p className="text-white font-weight-bold ls-2 ">
+                  Lorem, ipsum dolor.
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 ">
+              <div className="faq-wrapper p-4">
+                <h3 className="text-white fs-20 font-weight-bold ls-2 ">
+                  How do I join Wolf Proxies?
+                </h3>
+                <p className="text-white font-weight-bold ls-2 ">
+                  Lorem, ipsum dolor.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row my-4">
+            <div className="col-12 col-md-6 ">
+              <div className="faq-wrapper p-4">
+                <h3 className="text-white fs-20 font-weight-bold ls-2 ">
+                  How do I join Wolf Proxies?
+                </h3>
+                <p className="text-white font-weight-bold ls-2 ">
+                  Lorem, ipsum dolor.
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 ">
+              <div className="faq-wrapper p-4">
+                <h3 className="text-white fs-20 font-weight-bold ls-2 ">
+                  How do I join Wolf Proxies?
+                </h3>
+                <p className="text-white font-weight-bold ls-2 ">
+                  Lorem, ipsum dolor.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* social section */}
+      <section className="social-section mx-auto p-4 my-5">
+        <div className="container">
+          <h1 className="text-white fs-25 text-center ls-2">Social Media</h1>
+          <div className="d-flex justify-content-center my-4">
+            <div className="social-wrapper mx-4"></div>
+            <div className="social-wrapper mx-4"></div>
+          </div>
+        </div>
+      </section>
+      {/* footer */}
+      <footer className="footer d-flex p-4">
+        <div className="container d-flex justify-content-between align-items-center">
+          <div className="font-weight-bold ls-2 text-white">
+            <h1 className="text-white fs-22 ls-2 text-uppercase">
+              <img
+                src={require('../img/Logo-mini.svg').default}
+                alt=""
+                className="mr-3"
+              />
+              Wolf proxies
+            </h1>
+            <p className=" my-3 ">© 2021 WOLF LLC</p>
+            <p className="">All Rights Reserved</p>
+          </div>
+          <div className="font-weight-bold ls-2 text-white">
+            <h1 className="fs-20">LINKS</h1>
+            <p className="my-3">Terms Of Service</p>
+            <p>Privacy Policy</p>
+          </div>
+          <div className="font-weight-bold ls-2 text-white">
+            <h1 className="fs-20">SOCIALS</h1>
+            <p className="my-3">Twitter</p>
+            <p>Discord</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
